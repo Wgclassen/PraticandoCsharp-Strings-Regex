902874de8d41f6c36d4d413ac36febf0944c948c
Willian Classen <Wgclassen@gmail.com>
2025-08-05 18:54:35 -0300

    First Commit

---
+++ CSharp-Encontrando-Palavra-chave/CSharp-Encontrando/Program.cs
@@ -0,0 +1,11 @@
+﻿string palavraChave = "C#";
+Console.WriteLine($"A palavra chave é: {palavraChave}");
+Console.WriteLine("Digite um texto:");
+string textoDigitado = Console.ReadLine();
+if (textoDigitado.Contains(palavraChave))
+{
+    Console.WriteLine("A palavra-chave foi encontrada na pesquisa.");
+} else
+{
+    Console.WriteLine("A palavra-chave não foi encontrada.");
+}
