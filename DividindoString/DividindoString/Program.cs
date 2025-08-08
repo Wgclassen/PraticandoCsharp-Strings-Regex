4c622024652097e3a2891b903ab454e404b148d3
Willian Classen <Wgclassen@gmail.com>
2025-08-08 14:25:58 -0300

    Dividindo String

---
+++ DividindoString/DividindoString/Program.cs
@@ -0,0 +1,8 @@
+﻿string log = "2025-03-25,Erro,Arquivo não encontrado";
+var dividido = log.Split(',');
+string data = dividido[0];
+string erro =  dividido[1];
+string mensagem = dividido[2];
+Console.WriteLine(data);
+Console.WriteLine(erro);
+Console.WriteLine(mensagem);
