c09493c19db3e7e8e14ceeaf4049d21f568fae0c
Willian Classen <Wgclassen@gmail.com>
2025-08-10 18:14:20 -0300

    relatorio

---
+++ FormatandoRelatorio/FormatandoRelatorio/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Digite o nome do cliente:");
+string nomeCliente = Console.ReadLine();
+Console.WriteLine("Digite o endereço:");
+string enderecoCliente = Console.ReadLine();
+Console.WriteLine("Digite o valor do frete:");
+string valorFrete =  Console.ReadLine();
+Console.WriteLine("Digite a data de entrega:");
+string dataEntrega = Console.ReadLine();
+
+Console.WriteLine("\n========== RELATÓRIO DE ENTREGA ==========\n");
+Console.WriteLine($"Cliente: {nomeCliente}");
+Console.WriteLine($"Endereço: {enderecoCliente}");
+Console.WriteLine($"Valor do frete: {valorFrete}");
+Console.WriteLine($"Data: {dataEntrega}");
+Console.WriteLine("\n==========================================");
