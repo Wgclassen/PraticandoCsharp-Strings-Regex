34c7d38d7cdfab267e5cf814ec054c8e2b39f3ac
Willian Classen <Wgclassen@gmail.com>
2025-08-11 18:58:47 -0300

    RegexVerificador de Strings Numericas

---
+++ VerificadorStringsNumericas/VerificadorStringsNumericas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Digite o código do cupom:");
+string codigo = Console.ReadLine();
+string regex = @"^\d+$";
+
+bool valido = Regex.IsMatch(codigo, regex);
+
+if (valido)
+{
+    Console.WriteLine($"O código {codigo} é válido.");
+} else
+{
+    Console.WriteLine($"O código {codigo} não é válido.");
+}
