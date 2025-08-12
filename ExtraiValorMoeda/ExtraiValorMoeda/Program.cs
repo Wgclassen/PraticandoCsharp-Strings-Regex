331df49f8148aa8e901cadb8a7a644a39b005a36
Willian Classen <Wgclassen@gmail.com>
2025-08-12 17:24:41 -0300

    Valor Moeda

---
+++ ExtraiValorMoeda/ExtraiValorMoeda/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Digite o texto do recibo:");
+string recibo  = Console.ReadLine();
+string regex = @"R\$ \d+,\d{2}";
+string saida = Regex.Match(recibo, regex).Value;
+Console.WriteLine($"Valor encontrado: {saida}");
