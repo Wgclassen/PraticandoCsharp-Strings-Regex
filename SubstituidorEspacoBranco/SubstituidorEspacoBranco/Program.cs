981ca3c687cc0977ead67a586fa9d4fe3e87016a
Willian Classen <Wgclassen@gmail.com>
2025-08-13 16:22:32 -0300

    Whitespace replacer

---
+++ SubstituidorEspacoBranco/SubstituidorEspacoBranco/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Digite sua frase:");
+string entrada = Console.ReadLine();
+string mensagem = Regex.Replace(entrada, @"\s+", " ").Trim();
+Console.WriteLine(mensagem);
