69c72f0ca26ed417f9641361ac9f9f495e78f9db
Willian Classen <Wgclassen@gmail.com>
2025-08-14 17:58:29 -0300

    Date validator

---
+++ ValidantoDatas/ValidantoDatas/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Digite a data (dd/mm/aaaa):");
+string data = Console.ReadLine();
+string regex = @"^\d{2}/\d{2}/\d{4}$";
+
+if (Regex.IsMatch(data, regex))
+{
+    Console.WriteLine("A data está no formato correto.");
+} else
+{
+    Console.WriteLine("Formato inválido! Use dd/mm/aaaa.");
+}
