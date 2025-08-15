2e7ce46e6bb30566cd518d8be201fb134b69244d
Willian Classen <Wgclassen@gmail.com>
2025-08-15 02:15:00 -0300

    LinkReplacer

---
+++ LinkFormatter/LinkFormatter/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Digite o texto:");
+string texto = Console.ReadLine();
+string regex = @"https?://\S+";
+string textoNovo = Regex.Replace(texto, regex, "[LINK]");
+Console.WriteLine(textoNovo);
